820ee51c33c7b3a209d0432ec461a3059da66576
Justin Sanchez <justinlsanchez12@gmail.com>
2024-02-06 15:19:56 -0800

    removed comments of unnecessary paths

--- InteractiveTable/urls.py
+++ InteractiveTable/urls.py
@@ -1,11 +1,7 @@
-"""
-
-
-"""
 from django.urls import path
 from . import views
 urlpatterns = [
     path('interactive/', views.interactive_grid, name='interactive_grid'),
-    path('update/', views.update_grid, name="update_grid")
-    
+    path('confirmation/', views.confirmation, name='confirmation'),
+    path('update/', views.update_grid, name='update_grid'),
 ]
